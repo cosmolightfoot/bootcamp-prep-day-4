a0385f917dc63006cf1b8114d53906cb7baedd88
Cosmo Kent <cosmolightfoot@gmail.com>
2019-01-17 19:23:17 -0800

    mapped out greet function; assigned element id's

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 //eslint-disable-next-line
 function greet() {
     console.log('greet function called')
+    //get the inputs by id
+
+    //get the values of inputs
+    
+    //make the string of the greeting
+
+    //display the greeting
+
+    //remove prompt
 }
