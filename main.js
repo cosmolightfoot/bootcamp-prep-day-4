77d43bcb11f3d23326a1de388fc6864b612319ad
Cosmo Kent <cosmolightfoot@gmail.com>
2019-01-17 19:31:21 -0800

    function calls first and last name inputs to console

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 //eslint-disable-next-line
 function greet() {
-    console.log('greet function called')
-    //get the inputs by id
-
-    //get the values of inputs
-    
+    var firstName = document.getElementById('first-name').value;
+    var lastName = document.getElementById('last-name').value;
+    console.log(firstName);
+    console.log(lastName);
     //make the string of the greeting
 
     //display the greeting
